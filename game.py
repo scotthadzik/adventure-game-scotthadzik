8f6ba524c74ba98e2b808471bfe3134854fea1af
Scott Hadzik <scotthadzik@weber.edu>
2025-02-06 10:02:03 -0700

    REF: while loop for invalid input

--- game.py
+++ game.py
@@ -33,10 +33,18 @@ print(starting_area)
 # Ask the player for their first decision
 decision = input("Do you wish to take the path (yes or no): ").lower()
 
+# Invalid response loop until they give a valid
+while decision not in ["yes", "no"]:
+    print("Invalid choice. Please type 'yes' or 'no'.")
+    # option for the user to make a new decision
+    decision = input("Do you wish to take the path (yes or no): ").lower()
+
 # Respond based on the player's decision
 if decision == "yes":
     print(f"Brave choice, {player_name}! You step on the path and venture forward")
 elif decision == "no":
     print(f"{player_name} , you decide to wait. Perhaps courage will find you later.")
-else:
-    print("Confused, you stand still, unsure of what to do.")
+
+
+
+    
