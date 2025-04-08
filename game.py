50bc108baf6fd462d305b24f273b79b7f6a9fa21
Scott Hadzik <scotthadzik@weber.edu>
2025-04-08 08:51:00 -0600

    ADD: Person Class - option for cave

--- game.py
+++ game.py
@@ -2,7 +2,7 @@
 DOCSTRING
 Adventure Game
 Author: Scott Hadzik
-Version: 1.0
+Version: 2.0
 Description:
 This is a text-based adventure game where the player makes choices
 to navigate through a mysterious forest.
@@ -19,20 +19,6 @@ class Player:
         self.health = 100
         self.has_map = False
         self.has_lantern = False
-
-# TODO:  Add a check before certain choices
-#       - Change 
-#           - 3. Stay where you are. 
-#           to
-#           - 4. Stay where you are.
-#       - Create a new option  
-#           - 3. Enter the Cave 
-#       - Update 
-#           - You see two paths ahead:
-#           - There are multiple paths ahead:
-#       - If player.has_lantern is False, prevent entering a cave
-#       - Print a message  
-#           - “It’s too dark to continue without a lantern.”
 
 #----------------
 # Function: welcome_player
@@ -93,29 +79,46 @@ while True:
     print("\nYou see two paths ahead:")
     print("\t1. Take the left path into the dark woods.")
     print("\t2. Take the right path toward the mountain pass.")
-    print("\t3. Stay where you are.")
+    print("\t3. Explore a nearby cave")
+    print("\t4. Stay where you are.")
     print("\tType 'i' to view your inventory.")
 
-    decision = input("What will you do (1,2,3 or i): ").lower()
+    decision = input("What will you do (1,2,3,4 or i): ").lower()
 
+    # open the inventory
     if decision == 'i':
         print("Inventory", player.inventory)
         continue
 
+    # Take the left path -- Dark Woods
     if decision == "1":
         print(f"{player.name}, you step into the dark woods."
               "The trees whisper as walk deeper.")
         add_to_inventory("lantern")
         player.has_lantern = True
 
+    # Take the right path --Mountain
     elif decision == "2":
         print(f"{player.name}, you make your way "
               "towards the mountain pass, feeling "
               "the cold wind against your face.")
         add_to_inventory("map")
         player.has_map = True
-        
+    
+    # Enter the cave, only if they have lantern in inventory
     elif decision == "3":
+        # if they have lantern
+        if player.has_lantern:
+            print(f"{player.name}, bravely enter the dark cave")
+            print(f"Inside the cave, you find hidden treasure")
+        
+        # if no lantern
+        else:
+            print("It's too dark to explore the cave without a lantern")
+            print("Maybe you should find a light source.")
+
+    # Stay were you are
+    elif decision == "4":
         print("You stay still, listening to the " 
               "distant sounds of the forest")
     else: 
