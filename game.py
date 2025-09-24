0b42ec5feb9fb785b265bb968534e4ce6d8c3abf
Scott Hadzik <scotthadzik@weber.edu>
2025-09-24 11:06:45 -0600

    ADD: Player Name input and welcome message

--- game.py
+++ game.py
@@ -1,2 +1,10 @@
-print("Welcome to the Adventure Game!")  
+# Welcome message and introduction
+print("Welcome to the Adventure Game!")
 print('Your journey begins here... ')
+
+# Ask for the player's name  variable
+player_name = input("What is your name, adventurer? ")
+
+# Use an f-string to display 
+# the same message in a more readable way
+print(f"Welcome, {player_name}! Your journey begins now.")
