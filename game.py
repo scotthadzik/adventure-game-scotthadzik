636d23765f88db2fefefb0e49a3feffd72563cc9
Scott Hadzik <scotthadzik@weber.edu>
2026-01-15 11:13:42 -0700

    ADD: docstring for author information

--- game.py
+++ game.py
@@ -1,2 +1,11 @@
+"""
+Adventure Game
+Author: Scott Hadzik
+Version: 1.0
+Description:
+This is a text-based adventure game where the player makes choices
+to navigate through a mysterious forest.
+"""
+
 print("Welcome to the Adventure Game!")  
 print("Your journey begins here...")
